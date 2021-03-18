2ff85328afd9e6c5f122db352cc740dae3e95b46
Ketan Desai <desai.ketan@gmail.com>
2021-03-18 15:48:50 -0400

    added perspective effect.

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -30,6 +30,7 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+    <div className={styles.perspectiveEffect}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -87,6 +88,8 @@ function App() {
           skinColor={skinColor}
           clothesColor={clothesColor}
         />
+
+        
       </div>
     </main>
   );
